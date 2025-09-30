7e2b3917560083efcafd18cf3bcc549d489935f2
RomanSibakovskij <benutzer373@gmail.com>
2025-09-30 06:46:36 +0300

    Add empty RegisterPage sheet

---
+++ src/pages/register.page.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const {By} = require("selenium-webdriver");
+
+const BasePage = require("./utilities/base.page.js");
+
+class RegisterPage extends BasePage{
+
+    constructor(driver) {
+        super(driver);
+    }
+
+}
+module.exports = { RegisterPage };
