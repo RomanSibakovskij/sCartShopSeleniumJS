0b2e11c5f576aefdad8ab1265dff11205bc1a933
RomanSibakovskij <benutzer373@gmail.com>
2025-09-30 07:04:45 +0300

    Add relevant web elements for RegisterPageInvalidSingularInput utility sheet

--- src/pages/register-page-invalid-scenarios/register.page.invalid.singular.input.js
+++ src/pages/register-page-invalid-scenarios/register.page.invalid.singular.input.js
@@ -8,6 +8,17 @@ class RegisterPageInvalidSingularInput extends BasePage{
 
     constructor(driver) {
         super(driver);
+
+        //relevant web elements
+        this._registerPageFirstNameInputField = By.xpath("//div[@id='collapseExample']/div[1]/input");
+        this._registerPageLastNameInputField = By.xpath("//div[@id='collapseExample']/div[2]/input");
+        this._registerPageEmailInputField = By.xpath("//div[@id='collapseExample']/div[3]/input");
+        this._registerPagePhoneInputField = By.xpath("//div[@id='collapseExample']/div[4]/input");
+        this._registerPageAddressOneInputField = By.xpath("//div[@id='collapseExample']/div[5]/input");
+        this._registerPageAddressTwoInputField = By.xpath("//div[@id='collapseExample']/div[6]/input");
+        this._registerPagePasswordInputField = By.xpath("//div[@id='collapseExample']/div[8]/input");
+        this._registerPageConfirmPasswordInputField = By.xpath("//div[@id='collapseExample']/div[9]/input");
+
     }
 
 
