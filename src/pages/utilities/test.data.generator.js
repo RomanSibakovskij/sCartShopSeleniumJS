7bbf64ee834e9d95b88e9bc8f5e3f3232881bbc7
RomanSibakovskij <benutzer373@gmail.com>
2025-09-30 06:50:28 +0300

    Add empty TestDataGenerator utility sheet

---
+++ src/pages/utilities/test.data.generator.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const BasePage = require("./base.page.js");
+
+class TestDataGenerator extends BasePage{
+
+    constructor(driver) {
+        super(driver);
+    }
+
+}
+module.exports = TestDataGenerator;
