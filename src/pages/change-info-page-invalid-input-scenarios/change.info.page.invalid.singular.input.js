9bb9bc54bc2385a67b71424abbc51c0b9ac61e65
RomanSibakovskij <benutzer373@gmail.com>
2025-10-01 06:27:35 +0300

    Add empty ChangeInfoPageInvalidSingularInput utility sheet

---
+++ src/pages/change-info-page-invalid-input-scenarios/change.info.page.invalid.singular.input.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const {By} = require("selenium-webdriver");
+
+const BasePage = require("../utilities/base.page.js");
+
+class ChangeInfoPageInvalidSingularInput extends BasePage{
+
+    constructor(driver) {
+        super(driver);
+    }
+
+
+
+}
+module.exports = { ChangeInfoPageInvalidSingularInput };
