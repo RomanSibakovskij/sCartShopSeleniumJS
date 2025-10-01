9e190711d1edcf82a9b04523fd99ccdcf4d20ed8
RomanSibakovskij <benutzer373@gmail.com>
2025-10-01 06:28:45 +0300

    Add relevant web elements for ChangeInfoPageInvalidSingularInput utility sheet

--- src/pages/change-info-page-invalid-input-scenarios/change.info.page.invalid.singular.input.js
+++ src/pages/change-info-page-invalid-input-scenarios/change.info.page.invalid.singular.input.js
@@ -8,6 +8,14 @@ class ChangeInfoPageInvalidSingularInput extends BasePage{
 
     constructor(driver) {
         super(driver);
+
+        //relevant web elements
+        this._changeInfoFirstNameInputField = By.xpath("//input[@id='first_name']");
+        this._changeInfoLastNameInputField = By.xpath("//input[@id='last_name']");
+        this._changeInfoPhoneInputField = By.xpath("//input[@id='phone']");
+        this._changeInfoAddressOneInputField = By.xpath("//input[@id='address1']");
+        this._changeInfoAddressTwoInputField = By.xpath("//input[@id='address2']");
+
     }
 
 
