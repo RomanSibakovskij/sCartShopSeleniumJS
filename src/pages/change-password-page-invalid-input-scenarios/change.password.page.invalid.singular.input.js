803dfd8b25d9ced4f13b74a355f92d013844305d
RomanSibakovskij <benutzer373@gmail.com>
2025-10-01 16:45:41 +0300

    Add relevant web elements for ChangePasswordPageInvalidSingularInput utility sheet

--- src/pages/change-password-page-invalid-input-scenarios/change.password.page.invalid.singular.input.js
+++ src/pages/change-password-page-invalid-input-scenarios/change.password.page.invalid.singular.input.js
@@ -8,6 +8,12 @@ class ChangePasswordPageInvalidSingularInput extends BasePage{
 
     constructor(driver) {
         super(driver);
+
+        //relative web elements
+        this._changePasswordPageOldPasswordInputField = By.xpath("(//input[@type='password'])[1]");
+        this._changePasswordPageNewPasswordInputField = By.xpath("(//input[@type='password'])[2]");
+        this._changePasswordPageConfirmPasswordInputField = By.xpath("//input[@id='password-confirm']");
+
     }
 
 
