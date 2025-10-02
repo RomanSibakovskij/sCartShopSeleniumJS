8a253c0f8bbc267a9113c4a9f973ecc8bc5723f2
RomanSibakovskij <benutzer373@gmail.com>
2025-10-02 06:09:21 +0300

    Add relevant web elements for AddressDetailsPageInvalidSingularInput utility sheet

--- src/pages/address-details-page-invalid-input-scenarios/address.details.page.invalid.singular.input.js
+++ src/pages/address-details-page-invalid-input-scenarios/address.details.page.invalid.singular.input.js
@@ -8,6 +8,14 @@ class AddressDetailsPageInvalidSingularInput extends BasePage{
 
     constructor(driver) {
         super(driver);
+
+        //relevant web elements
+        this._addressDetailsPageFirstNameInputField = By.xpath("//input[@id='first_name']");
+        this._addressDetailsPageLastNameInputField = By.xpath("//input[@id='last_name']");
+        this._addressDetailsPagePhoneInputField = By.xpath("//input[@id='phone']");
+        this._addressDetailsPageAddressOneInputField = By.xpath("//input[@id='address1']");
+        this._addressDetailsPageAddressTwoInputField = By.xpath("//input[@id='address2']");
+
     }
 
 
