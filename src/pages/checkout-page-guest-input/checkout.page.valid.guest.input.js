6adb4c4f4f1598680310568181ef0e28c87cd959
RomanSibakovskij <benutzer373@gmail.com>
2025-10-03 13:38:33 +0300

    Add relevant web elements for CheckoutPageValidGuestInput utility sheet

--- src/pages/checkout-page-guest-input/checkout.page.valid.guest.input.js
+++ src/pages/checkout-page-guest-input/checkout.page.valid.guest.input.js
@@ -6,6 +6,17 @@ class CheckoutPageValidGuestInput extends BasePage{
 
     constructor(driver) {
         super(driver);
+
+        //relevant web elements
+        //input form
+        this._checkoutPageFirstNameInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[1]/td[1]/input");
+        this._checkoutPageLastNameInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[1]/td[2]/input");
+        this._checkoutPageEmailInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[2]/td[1]/input");
+        this._checkoutPagePhoneInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[2]/td[2]/input");
+        this._checkoutPageAddressOneInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[5]//input");
+        this._checkoutPageAddressTwoInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[6]//input");
+        this._checkoutPageNoteTextarea = By.xpath("//table[@class='table table-borderless table-responsive']//tr[7]//textarea");
+
     }
 
 
