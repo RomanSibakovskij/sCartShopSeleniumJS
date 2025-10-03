c92a4ebf2df15694b4a1f60144b2c4902edc0827
RomanSibakovskij <benutzer373@gmail.com>
2025-10-03 05:30:51 +0300

    Add empty WishlistPage sheet

---
+++ src/pages/wishlist.page.js
@@ -0,0 +1,15 @@
+const {By} = require("selenium-webdriver");
+
+const BasePage = require("./utilities/base.page.js");
+
+class WishlistPage extends BasePage{
+
+    constructor(driver) {
+        super(driver);
+    }
+
+
+
+
+}
+module.exports = { WishlistPage };
