f6aa2282d0806f55ca6e8bc0779d8777a59c681d
RomanSibakovskij <benutzer373@gmail.com>
2025-10-03 06:45:10 +0300

    Add Multiple New Products To Compare List Test (as a registered user) method

--- src/tests/compare.list.page.test.js
+++ src/tests/compare.list.page.test.js
@@ -40,6 +40,16 @@ describe('Compare List Page Tests', () => {
             await testMethods.addMultipleHomePageNewProductsToCompareListGuestTest();
         });
 
+        //Test 012c -> add multiple new products ("Product bundle 3 - English", "Sample product 1 - English", "Sample product 2 - English") to compare list test (as a registered user)
+        test("Add Multiple New Products To Compare List Test (as a registered user)", async function () {
+            //navigate user to register page test
+            await testMethods.navigateToRegisterPageTest();
+            //valid user account creation test
+            await testMethods.validUserAccountCreationTest();
+            //add multiple new products ("Product bundle 3 - English", "Sample product 1 - English", "Sample product 2 - English") to compare list test (as a registered user)
+            await testMethods.addMultipleHomePageNewProductsToCompareListRegUserTest();
+        });
+
     });
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/tests/utilities/test.methods.js
+++ src/tests/utilities/test.methods.js
@@ -8822,6 +8822,91 @@ class TestMethods extends BaseTest{
         await captureScreenshot(this.driver, "Multiple Products (Product bundle 1 - English, Sample product 5 - English, Sample product 6 - English) Addition To Compare List Test Result (guest)");
     }
 
+    //add multiple products ("Product bundle 3 - English", "Sample product 1 - English", "Sample product 2 - English") to compare list test method (as a registered user)
+    async addMultipleHomePageNewProductsToCompareListRegUserTest(){
+        const basePage = new BasePage(this.driver);
+        const generalPage = new GeneralPage(this.driver);
+        const generalPageTextElementAsserts = new GeneralPageTextElementAsserts(this.driver);
+        const homePage = new HomePage(this.driver);
+        const homePageTextElementAssert = new HomePageTextElementAssert(this.driver);
+        const homePageDataLogger = new HomePageDataLogger(this.driver);
+        const compareListPage = new CompareListPage(this.driver);
+        const compareListPageDataLogger = new CompareListPageDataLogger(this.driver);
+        //wait for elements to load
+        await basePage.waitForElementLoad(2000);
+        //general page web element assert
+        await generalPage.isGeneralPageWebElementDisplayed();
+        //general page header text element assert (registered user side)
+        await generalPageTextElementAsserts.isGeneralPageHeaderRegUserTextElementAsExpected();
+        //general page footer web element assert (Selenium can't find these elements with VALID selectors)
+        //await generalPage.isGeneralPageFooterWebElementDisplayed();
+        //general page footer text element assert (Selenium can't find these elements with VALID selectors)
+        //await generalPageTextElementAsserts.isGeneralPageFooterTextElementAsExpected();
+        //click home page logo
+        await generalPage.clickHeaderHomeLogoLink();
+        //wait for elements to load
+        await basePage.waitForElementLoad(2000);
+        //general page web element assert
+        await generalPage.isGeneralPageWebElementDisplayed();
+        //general page header text element assert (registered user side)
+        await generalPageTextElementAsserts.isGeneralPageHeaderRegUserTextElementAsExpected();
+        //general page footer web element assert (Selenium can't find these elements with VALID selectors)
+        //await generalPage.isGeneralPageFooterWebElementDisplayed();
+        //general page footer text element assert (Selenium can't find these elements with VALID selectors)
+        //await generalPageTextElementAsserts.isGeneralPageFooterTextElementAsExpected();
+        //scroll down to new products section
+        await homePage.scrollDownToNewProductsSection();
+        //wait for elements to load
+        await basePage.waitForElementLoad(2000)
+        //home page web element assert (Selenium can't find these elements with VALID selectors)
+        //await homePage.isHomePageWebElementDisplayed();
+        //home page text element assert
+        await homePageTextElementAssert.isHomePageTextElementAsExpected();
+        //log home page new product data
+        await homePageDataLogger.logHomePageNewProductData();
+        //capture screenshot of the home page display
+        await captureScreenshot(this.driver, "Home Page Display");
+        //hover over set new product ("Product bundle 3 - English") card
+        await homePage.hoverOverSetNewProductCard(2)
+        //click set new product ("Product bundle 3 - English") "Add to compare list" button
+        await homePage.clickSetNewProductAddToCompareListBtn(2);
+        //hover over set new product ("Sample product 1 - English") card
+        await homePage.hoverOverSetNewProductCard(6)
+        //click set new product ("Sample product 1 - English") "Add to compare list" button
+        await homePage.clickSetNewProductAddToCompareListBtn(6);
+        //hover over set new product ("Sample product 2 - English") card
+        await homePage.hoverOverSetNewProductCard(7)
+        //click set new product ("Sample product 2 - English") "Add to compare list" button
+        await homePage.clickSetNewProductAddToCompareListBtn(7);
+        //click header "Account" navbar link
+        await generalPage.clickSetNavBarLink(4);
+        //click "Compare" option
+        await generalPage.clickSetAccountDropdownMenuOption(3);
+        //wait for elements to load (due to network issues, wait time is extended)
+        await basePage.waitForElementLoad(4000);
+        //general page web element assert
+        await generalPage.isGeneralPageWebElementDisplayed();
+        //general page breadcrumb web element assert
+        await generalPage.isGeneralPageBreadcrumbWebElementDisplayed();
+        //general page footer web element assert (Selenium can't find these elements with VALID selectors)
+        //await generalPage.isGeneralPageFooterWebElementDisplayed();
+        //general page footer text element assert (Selenium can't find these elements with VALID selectors)
+        //await generalPageTextElementAsserts.isGeneralPageFooterTextElementAsExpected();
+        //general page header text element assert (registered user side)
+        await generalPageTextElementAsserts.isGeneralPageHeaderRegUserTextElementAsExpected();
+        //compare list page web element assert
+        await compareListPage.isCompareListPageWebElementDisplayed();
+        //log compare list page product data
+        await compareListPageDataLogger.logCompareListPageProductData();
+        //assert the correct products are added to compare list
+        const expectedCompareListProductNames = ["Product bundle 3 - English(SAMPLE-BUNDLE-3)", "Sample product 1 - English(SAMPLE-0-1)", "Sample product 2 - English(SAMPLE-0-2)"];
+        const productData = await compareListPage.getCompareListPageProductData();
+        const actualCompareListProductNames = productData.map(product => product[0]); //map to get just the first line (product name) from each product (since the product name is included in the div block and it can't be extracted otherwise)
+        assert.deepStrictEqual(expectedCompareListProductNames, actualCompareListProductNames, "The compare list page added product names don't match expectations");
+        //capture screenshot of the test result
+        await captureScreenshot(this.driver, "Multiple Products (Product bundle 3 - English, Sample product 1 - English, Sample product 2 - English) Addition To Compare List Test Result (registered user)");
+    }
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
