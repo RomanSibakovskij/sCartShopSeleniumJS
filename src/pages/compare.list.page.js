c01136a5b7145ad5d5a1bf24ece3a2b9da7f0649
RomanSibakovskij <benutzer373@gmail.com>
2025-10-03 06:09:22 +0300

    Add empty CompareListPage sheet

---
+++ src/pages/compare.list.page.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const {By} = require("selenium-webdriver");
+
+const BasePage = require("./utilities/base.page.js");
+
+class CompareListPage extends BasePage{
+
+    constructor(driver) {
+        super(driver);
+    }
+
+
+
+
+
+}
+module.exports = { CompareListPage };
