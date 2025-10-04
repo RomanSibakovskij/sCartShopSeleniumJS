2e7e36e36be1d1bd29d503c2825ef04c9412cf94
RomanSibakovskij <benutzer373@gmail.com>
2025-10-04 06:32:14 +0300

    Add relevant web elements for CheckoutPageInvalidSingularInput utility sheet

--- src/pages/checkout-page-invalid-input-scenarios/checkout.page.invalid.singular.input.js
+++ src/pages/checkout-page-invalid-input-scenarios/checkout.page.invalid.singular.input.js
@@ -1,11 +1,21 @@
 const {By} = require("selenium-webdriver");
 
 const BasePage = require("../utilities/base.page.js");
+const Logger = require("../utilities/logger");
 
 class CheckoutPageInvalidSingularInput extends BasePage{
 
     constructor(driver) {
         super(driver);
+
+        //relevant web elements
+        this._checkoutPageFirstNameInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[1]/td[1]/input");
+        this._checkoutPageLastNameInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[1]/td[2]/input");
+        this._checkoutPageEmailInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[2]/td[1]/input");
+        this._checkoutPagePhoneInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[2]/td[2]/input");
+        this._checkoutPageAddressOneInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[5]//input");
+        this._checkoutPageAddressTwoInputField = By.xpath("//table[@class='table table-borderless table-responsive']//tr[6]//input");
+
     }
 
 
